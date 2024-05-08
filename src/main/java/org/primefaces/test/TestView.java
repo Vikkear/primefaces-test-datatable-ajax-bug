83191e6e34379089b20a85652bca5a212e4be2d4
Victor Bonnier <victor.bonnier@telenor.se>
2024-05-08 15:08:02 +0200

    Added datatable with a removal button to replicate bug

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,6 +1,10 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
@@ -8,10 +12,11 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 
-import org.primefaces.model.JpaLazyDataModel;
 import org.primefaces.model.LazyDataModel;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
 @Named
@@ -24,15 +29,34 @@ public class TestView implements Serializable {
     private EntityManager em;
     private LazyDataModel<TestJpa> lazyDataModel;
 
+    @Getter
+    @Setter
+    private List<TestJpa> testJpaList;
+
     @PostConstruct
     public void init() {
         string = "Welcome to PrimeFaces!!!";
-
-        lazyDataModel = new JpaLazyDataModel<>(TestJpa.class, () -> em);
+        testJpaList = new ArrayList<TestJpa>();
+        testJpaList.add(createNewTestJpa(1L, "New York", 123L));
+        testJpaList.add(createNewTestJpa(2L, "Los Angeles", 124L));
+        testJpaList.add(createNewTestJpa(3L, "Philadelphia", 125L));
+        testJpaList.add(createNewTestJpa(4L, "Detroit", 126L));
+        testJpaList.add(createNewTestJpa(5L, "Boston", 127L));
+        testJpaList.add(createNewTestJpa(6L, "Borås", 128L));
     }
 
-    public LazyDataModel<TestJpa> getLazyDataModel() {
-        return lazyDataModel;
+    private TestJpa createNewTestJpa(Long id, String stringCol, Long numberCol) {
+        return TestJpa.builder()
+                .id(id)
+                .stringCol(stringCol)
+                .numberCol(numberCol)
+                .decimalCol(BigDecimal.valueOf(123.45))
+                .dateCol(new Date())
+                .build();
+    }
+
+    public void removeTestJpa(TestJpa testJpaToRemove) {
+        testJpaList.remove(testJpaToRemove);
     }
 
 }
